c0172098c725a77bd1b44abbf599beb377c84165
lidc <wx-lidc@dtdream.com>
2018-01-31 20:21:11 +0800

    解决异常未处理和增加了注释

--- src/main/java/com/cafababe/exec/example/ExecStarterMain.java
+++ src/main/java/com/cafababe/exec/example/ExecStarterMain.java
@@ -3,6 +3,7 @@ package com.cafababe.exec.example;
 import com.cafababe.exec.ExecStarter;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
 
 /**
  * @author cafababe
@@ -11,14 +12,18 @@ import java.io.IOException;
 public class ExecStarterMain {
 
     public static void main(String[] args) throws IOException {
-        for (int i = 0; i < 128; i++) {
-            if (i == 100) {
-                System.out.println(1);
-            }
-            ExecStarter.start("ping 127.0.0.1", new PrintDataHandler(), new PrintExceptionHandler());
+        for (int i = 0; i < 800; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        ExecStarter.start("ping 127.0.0.1", new PrintDataHandler(), new PrintExceptionHandler());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
         }
-        ExecStarter.shutdown();
-        ExecStarter.start("ping 127.0.0.1", new PrintDataHandler());
-        ExecStarter.shutdown();
     }
 }
