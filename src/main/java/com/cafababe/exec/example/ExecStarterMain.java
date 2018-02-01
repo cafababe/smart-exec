a3f52c2d30e7473780e515227b7785310684f3ae
lidc <wx-lidc@dtdream.com>
2018-02-01 20:20:42 +0800

    修改案例

--- src/main/java/com/cafababe/exec/example/ExecStarterMain.java
+++ src/main/java/com/cafababe/exec/example/ExecStarterMain.java
@@ -3,7 +3,6 @@ package com.cafababe.exec.example;
 import com.cafababe.exec.ExecStarter;
 
 import java.io.IOException;
-import java.util.concurrent.ExecutorService;
 
 /**
  * @author cafababe
@@ -12,18 +11,14 @@ import java.util.concurrent.ExecutorService;
 public class ExecStarterMain {
 
     public static void main(String[] args) throws IOException {
-        for (int i = 0; i < 800; i++) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        ExecStarter.start("ping 127.0.0.1", new PrintDataHandler(), new PrintExceptionHandler());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+        for (int i = 0; i < 300; i++) {
+            new Thread(() -> {
+                try {
+                    new ExecStarter().start("ping 127.0.0.1", new PrintDataHandler(), new PrintExceptionHandler());
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }).start();
-
         }
     }
 }
